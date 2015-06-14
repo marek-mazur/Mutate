5b8d168e5f19081e24e526fdf4ef887e733f84df
brettlangdon <brett@blangdon.com>
2015-06-14 18:09:09 -0400

    fix styles and auto detect source language

--- config/scripts/googletranslate/google_translate.py
+++ config/scripts/googletranslate/google_translate.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2
 #!encoding = utf-8
 
-import re
 import urllib
 import urllib2
 import json
@@ -9,47 +8,41 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-#text command icon subtext
-
-def has_chinese_charactar(content):
-    try:
-        iconvcontent = unicode(content)
-        zhPattern = re.compile(u'[\u4e00-\u9fa5]+')
-        match = zhPattern.search(iconvcontent)
-        res = False
-        if match:
-            res = True
-        return res
-    except:
-        return True
+# Statically set the language to translate into
+target_language = 'de'
 
 for i in range(1, len(sys.argv)):
-    if has_chinese_charactar(sys.argv[i]):
-        source_languege = "zh-CN"
-        target_languege = "en"
-    else:
-        source_languege = "en"
-        target_languege = "zh-CN"
     url = 'http://translate.google.cn/translate_a/t'
     payload = {
         'client': 'p',
-        'text'  : sys.argv[i],
-        'hl'    : 'en-EN',
-        'sl'    : source_languege,
-        'tl'    : target_languege,
+        'q': sys.argv[i],
+        'hl': 'en',
+        # Tell google to auto-detect the source language
+        'sl': 'auto',
+        'tl': target_language,
         'multires': '1',
-        'ssel'  : '0',
-        'tsel'  : '0',
-        'sc'    : '1',
-        'ie'    : 'UTF-8',
-        'oe'    : 'UTF-8'
-        }
-    response = urllib2.Request(url,urllib.urlencode(payload))
+        'ssel': '0',
+        'tsel': '0',
+        'sc': '1',
+        'ie': 'UTF-8',
+        'oe': 'UTF-8'
+    }
+    response = urllib2.Request(url, urllib.urlencode(payload))
     browser = "Mozilla/5.0 (Windows NT 6.1; WOW64)"
     response.add_header('User-Agent', browser)
     response = urllib2.urlopen(response)
     get_page = response.read()
     get_page = json.loads(get_page)
+
+    # Always show sentences first
+    for i in get_page['sentences']:
+        if 'trans' in i.keys():
+            print '[' + i['trans'] + ']'
+            print 'command=copy'
+            print 'icon='
+            print 'subtext=' + i['orig']
+
+    # If we have any exact translations, show those
     if 'dict' in get_page.keys() and 'entry' in  get_page['dict'][0]:
         for i in get_page['dict'][0]['entry']:
             print '[' + i['word'] + ']'
@@ -58,12 +51,5 @@ for i in range(1, len(sys.argv)):
             subtext = ''
             for tm in range(0, len(i['reverse_translation']) - 1):
                 subtext = subtext + i['reverse_translation'][tm] + ', '
-            subtext += i['reverse_translation'][-1] 
+            subtext += i['reverse_translation'][-1]
             print 'subtext=' + subtext
-    else:
-        for i in get_page['sentences']:
-            if 'trans' in i.keys():
-                print '[' + i['trans'] + ']'
-                print 'command=copy'
-                print 'icon='
-                print 'subtext=' + i['orig']
